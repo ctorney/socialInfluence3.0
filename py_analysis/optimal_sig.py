61c07050f430655959ae52da86bab9bf644484e7
Colin Torney <colin.j.torney@gmail.com>
2015-03-18 15:44:56 +0000

    linear approx to x1x2

--- py_analysis/optimal_sig.py
+++ py_analysis/optimal_sig.py
@@ -36,12 +36,57 @@ def EXTAPPROX(X,sig):
     B = np.log(K)
     VR = 0.5*B**2/float(Ns)
     return m.exp(-A)*0.5*m.exp((B*X))*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
+def EXTAPPROX2(X,sig):  
+    return 2*(2*X-1.0)/(2*X+1.0)
 def EXTAPPROX1(X,sig):  
-    A = np.log(K)/(2.0) - 1/sig
+    A = np.log(K)/(2.0) - 1/float(sig)
     B = np.log(K)
     VR = 0.5*B**2/float(Ns)
+    C = 0.5*B**2/float(Ns)
+    D = C*  (alpha*  (Ns-1) - 1.0)
+
     Y =  (A-m.log(B/2))/B
+#   Y= 0.1#A/B
+#  return m.exp(-A)*0.5*m.exp((B*Y))*(((1.0  - B*Y  +  0.5*B**2 * ( Y**2))  ) + X*(( B  +  0.5*B**2 * (-2*Y )) + (C   - C*B*Y  +  0.5*B**2 *C*( Y**2)) ) + X**2*((  0.5*B**2 ) + ( C*B  +  0.5*B**2 *C*(-2*Y ))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D )))
+#   return m.exp(-A)*0.5*m.exp((B*Y))*((1.0 +  B*X - B*Y  +  0.5*B**2 * (X**2-2*X*Y + Y**2)) + (C*X+  C*B*X**2 - C*B*Y*X  +  0.5*B**2 *C*X*(X**2-2*X*Y + Y**2))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D * X**2))
+    return -A + ((B*X))#*(1.0 +  B*(X-Y)  +  0.5*B**2 * (X-Y)**2)*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
+    return m.exp(-A)*0.5*m.exp((B*X))#*(1.0 +  B*(X-Y)  +  0.5*B**2 * (X-Y)**2)*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
+    return m.exp(-A)*0.5*m.exp((B*X))*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
+def SOLVEQUAD1(sig):  
+# ax^2 + bx + C = 0
+    A = np.log(K)/(2.0) - 1.0/float(sig)
+    B = np.log(K)
+    return 1.0/(2*m.exp(A) - B)
+    VR = 0.5*B**2/float(Ns)
+    C = 0.5*B**2/float(Ns)
+    D = C*  (alpha*  (Ns-1) - 1.0)
+
+    AB =   (1.0/(2.0*float(sig)*np.log(K)))#0.5 - 1.0/(sig*np.log(K))
+    AB1 =   - (1.0/(float(sig)*np.log(K)))#0.5 - 1.0/(sig*np.log(K))
+    Y =  (A-m.log(B/2))/B
+    qc = 1.0+  (1.0/(2.0*float(sig))) - 0.25*np.log(K)#1/B-0.5*(A/B)#((1.0  - B*Y  +  0.5*B**2 * ( Y**2))  )
+    qb =  (1.0/float(sig) - 2.0)#0.5 - AB - 2/B#(( B  +  0.5*B**2 * (-2*Y )) + (C   - C*B*Y  +  0.5*B**2 *C*( Y**2)) )-m.exp(A)*2.0*m.exp((-B*Y))
+    qa = np.log(K)#2*B#((  0.5*B**2 ) + ( C*B  +  0.5*B**2 *C*(-2*Y ))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D ))
+    return (-qb - m.sqrt(qb**2-4*qa*qc))/(2*qa)
+def SOLVEQUAD(sig):  
+# ax^2 + bx + C = 0
+    A = np.log(K)/(2.0) - 1.0/float(sig)
+    B = np.log(K)
+    return (1.0-A)/(2.0-B)
+    VR = 0.5*B**2/float(Ns)
+    C = 0.5*B**2/float(Ns)
+    D = C*  (alpha*  (Ns-1) - 1.0)
+
+    AB =   (1.0/(2.0*float(sig)*np.log(K)))#0.5 - 1.0/(sig*np.log(K))
+    AB1 =   - (1.0/(float(sig)*np.log(K)))#0.5 - 1.0/(sig*np.log(K))
+    Y =  (A-m.log(B/2))/B
+    qc = 1.0+  (1.0/(2.0*float(sig))) - 0.25*np.log(K)#1/B-0.5*(A/B)#((1.0  - B*Y  +  0.5*B**2 * ( Y**2))  )
+    qb =  (1.0/float(sig) - 2.0)#0.5 - AB - 2/B#(( B  +  0.5*B**2 * (-2*Y )) + (C   - C*B*Y  +  0.5*B**2 *C*( Y**2)) )-m.exp(A)*2.0*m.exp((-B*Y))
+    qa = np.log(K)#2*B#((  0.5*B**2 ) + ( C*B  +  0.5*B**2 *C*(-2*Y ))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D ))
+    return (-qb - m.sqrt(qb**2-4*qa*qc))/(2*qa)
  #   Y=A/B
+    return m.exp(-A)*0.5*m.exp((B*Y))*(((1.0  - B*Y  +  0.5*B**2 * ( Y**2))  ) + X*(( B  +  0.5*B**2 * (-2*Y )) + (C   - C*B*Y  +  0.5*B**2 *C*( Y**2)) ) + X**2*((  0.5*B**2 ) + ( C*B  +  0.5*B**2 *C*(-2*Y ))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D )))
+    return m.exp(-A)*0.5*m.exp((B*Y))*((1.0 +  B*X - B*Y  +  0.5*B**2 * (X**2-2*X*Y + Y**2)) + (C*X+  C*B*X**2 - C*B*Y*X  +  0.5*B**2 *C*X*(-2*X*Y + Y**2))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D * X**2))
     return m.exp(-A)*0.5*m.exp((B*Y))*(1.0 +  B*(X-Y)  +  0.5*B**2 * (X-Y)**2)*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
     return m.exp(-A)*0.5*m.exp((B*X))*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
 numA = 15
@@ -51,17 +96,21 @@ atimes2 = np.zeros(numA)
 alpha = 0.0
 
 dx = 0.01
-xs = np.arange(0,0.6,dx)
+xs = np.arange(0,1.0,dx)
 
 
 sigma = 4;
 ex = [EXTAPPROX(x,sigma) for x in xs]
 ex1 = [EXTAPPROX1(x,sigma) for x in xs]
+ex2 = [EXTAPPROX2(x,sigma) for x in xs]
 #for p in pot: print p
-plt.plot(xs,ex,linestyle='-')
-plt.plot(xs,ex1,linestyle='-')
-plt.plot(xs,xs,linestyle='-')
-plt.axis([0, 0.6, 0, 0.60])
+x2 = SOLVEQUAD1(sigma)
+#plt.plot(xs,ex,linestyle='-')
+plt.plot(xs,(ex1),linestyle='-')
+plt.plot(xs,(ex2),linestyle='-')
+plt.plot(x2, np.log(2*x2),'ro')
+plt.plot(xs,np.log(2.0*xs),linestyle='-')
+plt.axis([0, 1.0, -5.0, 1])
 plt.show()
 #atimes = (np.log(atimes))
 #plt.plot(sigs, atimes)
