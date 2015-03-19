ef2a9b7bf5c69beb7638fcf20498c3a051c433da
ctorney <colin.j.torney@gmail.com>
2015-03-19 08:43:45 +0000

    working approximation to x2

--- py_analysis/optimal_sig.py
+++ py_analysis/optimal_sig.py
@@ -11,7 +11,7 @@ from scipy import integrate
 from scipy import optimize
 
 NA = 128
-Ns =800
+Ns =18
 K = 90       
 ws = 0.524
 
@@ -32,7 +32,7 @@ def tup(X,sig):
 def tdown(X,sig): 
     return (X)*(1.0-EXTSWUP(X,sig))
 def EXTAPPROX(X,sig):  
-    A = np.log(K)/(2.0) - 1/sig
+    A = np.log(K)/(2.0) - 1/float(sig)
     B = np.log(K)
     VR = 0.5*B**2/float(Ns)
     return m.exp(-A)*0.5*m.exp((B*X))*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
@@ -89,11 +89,17 @@ def SOLVEQUAD(sig):
     return m.exp(-A)*0.5*m.exp((B*Y))*((1.0 +  B*X - B*Y  +  0.5*B**2 * (X**2-2*X*Y + Y**2)) + (C*X+  C*B*X**2 - C*B*Y*X  +  0.5*B**2 *C*X*(-2*X*Y + Y**2))  + (1.0   - B*Y  +  0.5*B**2 * (Y**2))*( D * X**2))
     return m.exp(-A)*0.5*m.exp((B*Y))*(1.0 +  B*(X-Y)  +  0.5*B**2 * (X-Y)**2)*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
     return m.exp(-A)*0.5*m.exp((B*X))*(1.0 +  VR*X  +  ((VR)*(alpha*  (Ns-1) - 1.0)) * X**2)
+def fullX(X,sig):  
+# ax^2 + bx + C = 0
+    A = np.log(K)/(2.0) - 1.0/float(sig)
+    B = np.log(K)
+    return X*(1- ((B**2*X)/(2.0*(B*X-1)))*(alpha*X + (1-X)/Ns ))
+    return (1.0-A)/(2.0-B)
 numA = 15
 sigs = np.zeros(numA)
 atimes = np.zeros(numA)
 atimes2 = np.zeros(numA)
-alpha = 0.0
+alpha = 0.12
 
 dx = 0.01
 xs = np.arange(0,1.0,dx)
@@ -104,13 +110,13 @@ ex = [EXTAPPROX(x,sigma) for x in xs]
 ex1 = [EXTAPPROX1(x,sigma) for x in xs]
 ex2 = [EXTAPPROX2(x,sigma) for x in xs]
 #for p in pot: print p
-x2 = SOLVEQUAD1(sigma)
-#plt.plot(xs,ex,linestyle='-')
-plt.plot(xs,(ex1),linestyle='-')
-plt.plot(xs,(ex2),linestyle='-')
-plt.plot(x2, np.log(2*x2),'ro')
-plt.plot(xs,np.log(2.0*xs),linestyle='-')
-plt.axis([0, 1.0, -5.0, 1])
+x2 = SOLVEQUAD(sigma)
+x2 = fullX(x2,sigma)
+plt.plot(xs,ex,linestyle='-')
+#plt.plot(xs,(ex1),linestyle='-')
+plt.plot(x2, x2,'ro')
+plt.plot(xs,xs,linestyle='-')
+plt.axis([0, 0.5, 0, 0.5])
 plt.show()
 #atimes = (np.log(atimes))
 #plt.plot(sigs, atimes)
